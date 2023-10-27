fcd5eaad08fe49eb47799c7cac8973b204894090
austin <143038805+aames2@users.noreply.github.com>
2023-10-26 21:09:58 -0400

    tests for viewing isolated movie and movie by id

--- tests/unit/test_get_all_movies.py
+++ tests/unit/test_get_all_movies.py
@@ -1,16 +1,17 @@
 import pytest
 from src.models.movie import Movie
 
-class TestMovie(unittest.TestCase):
 
+class TestMovie(unittest.TestCase):
     def test_movie_creation(self):
         title = "Inception"
         rating = 5
 
-        movie = Movie( title, rating)
+        movie = Movie(title, rating)
 
         self.assertEqual(movie.title, title)
         self.assertEqual(movie.rating, rating)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     pytest.main()
