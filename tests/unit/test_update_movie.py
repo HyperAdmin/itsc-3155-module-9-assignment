7b0de4316d33f08338288f7356e9097520cf55d2
Will Quinn <wquinn5262@gmail.com>
2023-10-27 19:03:01 -0400

    Committing implement_edit_movies

--- tests/unit/test_update_movie.py
+++ tests/unit/test_update_movie.py
@@ -6,12 +6,37 @@ from src.models.movie import Movie
 def test_update_movie_pass():
     movie = get_movie_repository()
     movie.clear_db()
+
     movie.create_movie('Inception', 'Chrostoper Nolan', 5)
     movie.create_movie('Interstellar', 'Chrostoper Nolan', 5)
     movie.create_movie('Jurassic Park', 'Steven Spielberg', 5)
+
     movie_to_be_edited = movie.get_movie_by_title('Inception')
-    movie_to_be_edited = movie.update_movie('Justice League', 'Zack Snyder', 2)
-    
+    movie.update_movie(movie_to_be_edited.movie_id, 'Justice League', 'Zack Snyder', 2)
+    movie_to_be_edited = movie.get_movie_by_title('Justice League')
+
+    assert movie_to_be_edited.title == 'Justice League'
+    assert movie_to_be_edited.director == 'Zack Snyder'
+    assert movie_to_be_edited.rating == 2
+
+    movie.clear_db()
 
 def test_update_movie_fail():
     movie = get_movie_repository()
+    movie.clear_db()
+
+    movie.create_movie('Inception', 'Chrostoper Nolan', 5)
+    movie.create_movie('Interstellar', 'Chrostoper Nolan', 5)
+    movie.create_movie('Jurassic Park', 'Steven Spielberg', 5)
+
+    movie_to_be_edited = movie.get_movie_by_title('Inception')
+    movie.update_movie(movie_to_be_edited.movie_id, 'Justice League', 'Zack Snyder', 2)
+    movie_to_be_edited = movie.get_movie_by_title('Justice League')
+
+    try:
+        movie.update_movie(0, 'Justice League', 'Zack Snyder', 2)
+        assert False
+    except(ValueError):
+        assert True
+
+    movie.clear_db()
