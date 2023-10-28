c9b8e6598c29202ca560c003d72cc7948e1a17b8
Marc_Burns <marcospizza23@gmail.com>
2023-10-27 21:17:38 -0400

    Finished Movie List unit Tests

--- tests/unit/test_get_all_movies.py
+++ tests/unit/test_get_all_movies.py
@@ -1,1 +1,18 @@
-# TODO: Feature 1
+import pytest
+from src.models.movie import Movie
+
+def test_movie_creation():
+    movie_id = 65354
+    title = "Inception"
+    director = "Christopher Nolan"
+    rating = 5
+
+    movie = Movie(movie_id, title, director, rating)
+
+    assert movie.movie_id == movie_id
+    assert movie.title == title
+    assert movie.director == director
+    assert movie.rating == rating
+
+if __name__ == '__main__':
+    pytest.main()
